5faa423a094ad479bc033d96d44831a6a3ec7d3a
Aleksandar <aleksandartrajkovski460@gmail.com>
2024-12-04 22:06:42 +0100

    delivery 5.0-reg-log-needtofixbugs

---
+++ scripts/LineHead.js
@@ -0,0 +1,15 @@
+//black bar under header
+window.addEventListener("scroll", function () {
+    const transparentElement = document.querySelector(".transparent");
+
+    // Show the transparent element when scrolling down
+    if (window.scrollY > 50) { // Adjust this value as needed
+        transparentElement.style.opacity = "1";
+    } else {
+        transparentElement.style.opacity = "0";
+    }
+});
+
+
+
+
